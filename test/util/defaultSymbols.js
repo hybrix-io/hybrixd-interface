5357262173a743095f7ce52d828320dbd6a4d875
Rouke Pouw <roukepouw@gmail.com>
2020-08-12 08:49:59 +0000

    Issue/1284 Fix host reports

--- test/util/defaultSymbols.js
+++ test/util/defaultSymbols.js
@@ -1,36 +1,36 @@
-  const DEFAULT_TEST_SYMBOLS = [
-    'bch',
-    'dummy',
-    'eth',
-    'flo',
-    'ark',
-    'btc',
-    'burst',
-    'dash',
-    'dgb',
-    'etc',
-    // 'exp', depreciated
-    'lsk',
-    'ltc',
-    'nxt',
-    'omni',
-    'rise',
-    'shift',
-    'ubq',
-    'waves',
-    'xcp',
-    'xem',
-    'xrp',
-    'zec',
-    'mock.btc',
-    'eth.xhy',
-    'waves.xhy',
-    'nxt.xhy',
-    'omni.xhy',
-    'xcp.xhy',
-    'xem.xhy'
-    // 'bts' -> FAUCET, ETC!
-    // 'xel' -> HOST issues
-  ];
+const DEFAULT_TEST_SYMBOLS = [
+  'bch',
+  'dummy',
+  'eth',
+  'flo',
+  'ark',
+  'btc',
+  'burst',
+  'dash',
+  'dgb',
+  'etc',
+  // 'exp', depreciated
+  'lsk',
+  'ltc',
+  'nxt',
+  'omni',
+  'rise',
+  'shift',
+  'ubq',
+  'waves',
+  'xcp',
+  'xem',
+  'xrp',
+  'zec',
+  'mock.btc',
+  'eth.xhy',
+  'waves.xhy',
+  'nxt.xhy',
+  'omni.xhy',
+  'xcp.xhy',
+  'xem.xhy'
+  // 'bts' -> FAUCET, ETC!
+  // 'xel' -> HOST issues
+];
 
-  exports.DEFAULT_TEST_SYMBOLS = DEFAULT_TEST_SYMBOLS;
+exports.DEFAULT_TEST_SYMBOLS = DEFAULT_TEST_SYMBOLS;
--- test/util/render.js
+++ test/util/render.js
@@ -1,35 +1,45 @@
-const issueLib = require('./issue')
+const issueLib = require('./issue');
 
-function stringify(x){
+const renderSymbol = (renderCell, symbol, data, messages, newMessages, isTestingHosts) => {
+  const results = [];
+  for (let testId in data) {
+    const x = data[testId];
+    const valid = x.valid;
+    const known = x.known;
+    const result = x.result;
+    const testResult = renderCell(symbol, testId, valid, known, result, messages, newMessages, isTestingHosts);
+    results.push(testResult);
+  }
+  return results;
+};
+
+function stringify (x) {
   return typeof x === 'undefined' ? 'undefined' : JSON.stringify(x);
 }
 
 const renderTableJSON = data => {
   return stringify(data);
-}
+};
 
-const renderTableXML = (renderSymbol) => (data, isTestingHosts) => {
+const renderTableXML = (data, isTestingHosts) => {
   const messages = [];
   const newMessages = [];
 
-  let r='';
+  let r = '';
   for (let symbol in data.assets) {
-      r = renderSymbol(renderCellXML, symbol, data.assets[symbol],messages,newMessages, isTestingHosts).join('');
+    r += renderSymbol(renderCellXML, symbol, data.assets[symbol], messages, newMessages, isTestingHosts).join('');
   }
-  r='<?xml version="1.0" encoding="UTF-8" ?><testsuites id="hybrix" name="hybrix" tests="'+data.total+'" failures="'+(data.failures)+'" time="0.001"><testsuite id="testsuite.hybrix" name="hybrix" tests="'+data.total+'" failures="'+(data.failures)+'" time="0.001">'+r;
-  r+='</testsuite></testsuites>';
+  r = '<?xml version="1.0" encoding="UTF-8" ?><testsuites id="hybrix" name="hybrix" tests="' + data.total + '" failures="' + (data.failures) + '" time="0.001"><testsuite id="testsuite.hybrix" name="hybrix" tests="' + data.total + '" failures="' + (data.failures) + '" time="0.001">' + r;
+  r += '</testsuite></testsuites>';
   return r;
-}
+};
 
-const renderCellXML = (symbol,testId, valid, known, result, messages,newMessages, isTestingHosts) => {
+const renderCellXML = (symbol, testId, valid, known, result, messages, newMessages, isTestingHosts) => {
   const title = stringify(result).replace(/"/g, '');
-  const type_ = isTestingHosts ? Object.keys(result)[0] : testId;
-  const failureMsg = valid ? '<failure message="${title}" type="ERROR"></failure>' : ''
-
+  const type_ = testId;
+  const failureMsg = valid ? '' : `<failure message="${title}" type="ERROR"></failure>`;
   return `
-    <testcase id="${symbol + '_' + type_ + ' host'}" name="${symbol+' '+type_ + ' host'}" time="0.001">
-      ${failureMsg}
-    </testcase>`
+<testcase id="${symbol + '_' + type_ + ' host'}" name="${symbol + '_' + testId}" time="0.001">${failureMsg}</testcase>`;
 };
 
 const renderCellWeb = (symbol, type, validity, known, data, messages, newMessages, isTestingHosts) => {
@@ -39,16 +49,16 @@ const renderCellWeb = (symbol, type, validity, known, data, messages, newMessage
   const issueText = symbol + ' ' + text;
   const issueTextDashed = symbol + '_' + text;
   const known_ = known || {};
-  const message = isTestingHosts ? text : (known_.message || undefined)
+  const message = isTestingHosts ? text : (known_.message || undefined);
 
   if (known) {
     const color = valid ? 'purple' : 'orange';
-    messages.push(mkIssueLink(symbol, text, known_, message, known_.link, issueText, issueTextDashed, isTestingHosts))
-    return mkCell(color, title, issueTextDashed, issueText, isTestingHosts)
+    messages.push(mkIssueLink(symbol, text, known_, message, known_.link, issueText, issueTextDashed, isTestingHosts));
+    return mkCell(color, title, issueTextDashed, issueText, isTestingHosts);
   } else if (valid) {
     return `<td style="text-align:center;background-color:green" title="${title}">&nbsp;</td>`;
   } else {
-    newMessages.push(mkIssueLink(symbol, text, known_, title, known_.link, issueText, issueTextDashed, isTestingHosts))
+    newMessages.push(mkIssueLink(symbol, text, known_, title, known_.link, issueText, issueTextDashed, isTestingHosts));
     return mkCell('red', title, issueTextDashed, issueText, isTestingHosts);
   }
 };
@@ -69,11 +79,39 @@ const mkIssueLink = (symbol, type, known, message, link, issueText, issueTextDas
     </a>
     ${linkTag}
   `;
-}
+};
 
 const mkCell = (color, title, link, text, isHost) => `<td style="text-align:center;background-color:${color}" title="${title}"><a style="text-decoration:none; width: 100%;height: 100%;display: block;" href="#${link}">&nbsp;</a>${isHost ? '<span class="hostName">' + text + '</span>' : ''}</td>`;
 
+const renderCellCLI = (symbol,testId, valid, known, result, messages,newMessages) => {
+  const title = stringify(result).replace(/"/g, '');
+  if(known){
+    if(valid){
+      if(known.link){
+        messages.push('\033[36m'+symbol+ ' '+testId+'\033[0m : '+known.message);
+      }else{
+        messages.push('\033[36m'+symbol+ ' '+testId+'\033[0m : '+known.message+ ' \033[31m [Create issue]\033[0m');
+      }
+      return '\033[36m OK \033[0m';
+    }else{
+      if(known.link){
+        messages.push('\033[33m'+symbol+ ' '+testId+'\033[0m : '+known.message+' (Returned '+ title+')');
+      }else{
+        messages.push('\033[33m'+symbol+ ' '+testId+'\033[0m : '+known.message+ ' \033[31m [Create issue]\033[0m');
+      }
+      return '\033[33mWARN\033[0m';
+    }
+  } else if (valid) {
+    return '\033[32m OK \033[0m';
+  } else {
+    newMessages.push('\033[31m'+symbol+ ' '+testId+'\033[0m : returned '+title+' \033[31m [Create issue]\033[0m');
+    return '\033[31mFAIL\033[0m';
+  }
+};
+
 exports.xml = renderTableXML;
 exports.json = renderTableJSON;
-exports.renderCellWeb = renderCellWeb
-exports.stringify = stringify
+exports.renderCellWeb = renderCellWeb;
+exports.stringify = stringify;
+exports.renderSymbol = renderSymbol;
+exports.renderCellCLI = renderCellCLI;
